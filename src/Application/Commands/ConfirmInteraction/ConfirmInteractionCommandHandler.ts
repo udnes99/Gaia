b291ee7ab6d07e8b084651f38e30cbd13e147053
Henrik Udnes <henrik@udnes.dev>
2023-06-17 16:29:31 +0200

    Added extra check to ensure the confirmer is a part of the interaction.

--- src/Application/Commands/ConfirmInteraction/ConfirmInteractionCommandHandler.ts
+++ src/Application/Commands/ConfirmInteraction/ConfirmInteractionCommandHandler.ts
@@ -16,6 +16,8 @@ export default class ConfirmInteractionCommandHandler implements ICommandHandler
     public async handle(command: ConfirmInteractionCommand): Promise<void> 
     {
         const interaction = this.serializer.deserialize(Interaction, command.interaction);
+        if(![interaction.to, interaction.from].includes(this.context.organization))
+            throw new Error("Interaction can only be confirmed by an organization involved.")
         if(!await this.interactionRepo.equalsCurrentVersion(interaction))
             throw new Error("The provided interaction does not match the current version.");
         interaction.confirm(this.context.organization);
--- src/Application/Commands/ModifyInteraction/ModifyInteractionCommandHandler.ts
+++ src/Application/Commands/ModifyInteraction/ModifyInteractionCommandHandler.ts
@@ -17,6 +17,8 @@ export default class ModifyInteractionCommandHandler implements ICommandHandler<
     public async handle(command: ModifyInteractionCommand): Promise<void> 
     {
         const interaction = this.serializer.deserialize(Interaction, command.interaction);
+        if(![interaction.to, interaction.from].includes(this.context.organization))
+            throw new Error("Interaction can only be confirmed by an organization involved.")
         if(!await this.interactionRepo.equalsCurrentVersion(interaction))
             throw new Error("The provided interaction does not match the current version.");
         const modification = this.serializer.deserialize(InteractionModification, command.modification);
